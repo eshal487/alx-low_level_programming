0d0f6bb06dd4f2a59396b18ec0fd0741d0362a4c
eshal <eshalmk2@eexample.com>
2024-09-11 10:22:14 -0700

    size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+ #!/bin/bash
+ int main(void){printf("char: %lu\nint: %lu\nlong int: %lu\nlong long: %lu\nfloat:%lu\n",sizeof(char),sizeof(int),sizeof(long),sizeof(long long),sizeof(float)):return 0:} 
