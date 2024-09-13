4bd43d6e87196c25c090b6d395e048aac56feb96
eshal <eshalmk2@eexample.com>
2024-09-13 11:50:00 -0700

    print a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
- #/bin/bash
- int main(void){puts("\"programming is like building a multilingual puzzle"):return 0:} 
+ #include<stdio.h>
+/**
+ * main - Enty point
+ *
+ * return: Alwats 0 (successs)
+  int main(void
+  }
+         puts("\"programming is like building a multilingual puzzle");
+	 	 return (0)
+   }
