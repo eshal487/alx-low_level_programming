dddccddd4f9a38829cce1f764b9ab9729b5c8ac2
eshal <eshalmk2@eexample.com>
2024-09-23 13:30:56 -0700

    alphabet in lowercase"

---
+++ 0x01-variable_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * main - prints the alphabet.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdeefghijklmnopqrstuvwxyz";
+
+	int 1;
+
+	for (i = 0; 1 < 26; i++)
+	{
+		putchar(alp[1]);
+	}
+	putchair("\n");
+
+	return (0);
+}
+
+
