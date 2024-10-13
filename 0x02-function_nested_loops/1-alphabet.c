0ee3c6e1822f69c3bc833e552b9dbbbe5a365807
eshal <eshalmk2@eexample.com>
2024-10-12 20:31:36 -0700

    header file

---
+++ 0x02-function_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+void print_alphabet(void){
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	for (int i=0; i < 26; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('n');
+}
+int main (void)
+{
+	print_alphabet():
+	return 0;
+
