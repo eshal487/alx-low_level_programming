4aafef0327e6d2743089ba8e90315527f1d42602
eshal <eshalmk2@eexample.com>
2024-10-13 18:34:37 -0700

    function

---
+++ 0x02-function_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+int _islower(int c) {
+	if (c >= 'a' && c <=  'z') {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+int main() {
+	int _islower(int 
