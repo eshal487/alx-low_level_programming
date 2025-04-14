56adde5914ecb109b4a1857b90b67eca7c100cac
eshal <eshalmk2@eexample.com>
2025-04-14 10:42:21 -0700

    return no of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in alinked list
+ * @h: head
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+		}
+	return (count);
+}
+
+
